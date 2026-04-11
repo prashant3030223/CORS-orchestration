3381159c9f7c08a79b9a427b6495257ddbf4c799
prachi <prachi29003@gmail.com>
2026-04-12 00:33:39 +0530

    chore: code cleanup and improvements

--- backend/src/routes/apiRoutes.js
+++ backend/src/routes/apiRoutes.js
@@ -4,9 +4,13 @@ const apiController = require('../controllers/apiController');
 
 const { protect } = require('../middleware/authMiddleware');
 
+// Get all APIs for logged-in user
 router.get('/', protect, apiController.getApis);
+// Create new API entry
 router.post('/', protect, apiController.createApi);
+// Update API by ID
 router.put('/:id', protect, apiController.updateApi);
+// Delete API by ID
 router.delete('/:id', protect, apiController.deleteApi);
 
 module.exports = router;
